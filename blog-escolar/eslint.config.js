3bdd6cd155116e0dd7ce6e2059a8027b3bd8fc8e
David Alexandre Fernandes <davidalexandrefernandes@Outlook.com>
2026-01-09 01:41:26 -0300

    Fix eslint flat config composition

--- blog-escolar/eslint.config.js
+++ blog-escolar/eslint.config.js
@@ -7,14 +7,12 @@ export default [
   {
     ignores: ['dist'],
   },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.vite,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
